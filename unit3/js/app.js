15021f3e755d08f84861ee781964df6a3c4399bd
Angel Hurtado <angelxehg@outlook.com>
2020-04-28 17:34:22 -0500

    Two cars

--- unit3/js/app.js
+++ unit3/js/app.js
@@ -6,4 +6,21 @@ class Vehicle {
         this.model = model;
         this.color = color;
     }
+
+    // Methods
+    turnOn() {
+        // TODO
+        return this.brand + ": Engine ON";
+    }
+
+    turnOff() {
+        // TODO
+        return this.brand + ": Engine OFF";
+    }
 }
+
+sedan = new Vehicle("BMW", "i3 2020", "Red");
+console.log(sedan);
+
+vocho = new Vehicle("VW", "1980", "Green");
+console.log(vocho);
