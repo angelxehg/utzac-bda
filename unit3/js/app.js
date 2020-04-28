6493131bdac86973480f0da50a88cb8e79c0c5af
Angel Hurtado <angelxehg@outlook.com>
2020-04-28 17:43:56 -0500

    New inerit class

--- unit3/js/app.js
+++ unit3/js/app.js
@@ -10,17 +10,29 @@ class Vehicle {
     // Methods
     turnOn() {
         // TODO
-        return this.brand + ": Engine ON";
+        return this.color + " " + this.brand + " " + this.model + ": Engine ON";
     }
 
     turnOff() {
         // TODO
-        return this.brand + ": Engine OFF";
+        return this.color + " " + this.brand + " " + this.model + ": Engine OFF";
     }
 }
 
 sedan = new Vehicle("BMW", "i3 2020", "Red");
 console.log(sedan);
+console.log(sedan.turnOn());
 
 vocho = new Vehicle("VW", "1980", "Green");
 console.log(vocho);
+console.log(sedan.turnOff());
+
+class Truck extends Vehicle {
+
+    constructor(brand, model, color) {
+        super(brand, model, color);
+        console.log(this.brand + " truck turn on!");
+    }
+}
+
+voltron = new Truck("H1", "X2020", "Yellow");
